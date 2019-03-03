e2a703657bf2473603ae9801a507519b17f63dea
nikolaiev <Vladyslav.Nikolaiev@gmail.com>
2019-03-03 14:15:17 +0200

    Project updated

--- src/main/java/com/kubernetes/basket/BasketApplication.java
+++ src/main/java/com/kubernetes/basket/BasketApplication.java
@@ -1,4 +1,4 @@
-package com.kubernetes.basket.demo;
+package com.kubernetes.basket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
